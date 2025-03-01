d9a3b07df28ed02657100e497453c815463e6046
Robotgiggle <purplemrb12@gmail.com>
2025-02-28 19:26:21 -0500

    Randomize fruit texture

--- Scripts/fruit_controller.gd
+++ Scripts/fruit_controller.gd
@@ -6,18 +6,14 @@ var launch_force = 2000
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	input_pickable = true
+	$Sprite2D.set_frame(randi() % 10)
+	
 	var launch_dir = randf_range(-0.25, 0.25)
 	var launch_vector = Vector2(launch_dir, -1)
 	var offset = Vector2(randf()/150, 0) * -sign(launch_dir)
 	self.apply_impulse(launch_vector*launch_force*randf_range(0.9, 1.1), offset)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
-
-
 func _on_mouse_entered() -> void:
 	hide()
 	$CollisionShape2D.set_deferred("disabled", true)
-	# TODO: cut effects, add points
+	# TODO: detect mouse speed, cut effects, add points
