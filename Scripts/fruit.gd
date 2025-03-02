556199a39817fde6c7fcfe007a402c0e48cb0ea5
Robotgiggle <purplemrb12@gmail.com>
2025-03-01 23:41:53 -0500

    Store fruit type for use in splat anim

--- Scripts/fruit.gd
+++ Scripts/fruit.gd
@@ -1,8 +1,11 @@
 extends "res://Scripts/launchable.gd"
 
+var fruit_type: int
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Sprite2D.frame = randi_range(0,9)
+	fruit_type = randi_range(0,9)
+	$Sprite2D.frame = fruit_type
 	super()
 
 func slice_fx() -> void:
