0c53a4f6deb44b287ff5ce053c2dbbdce08869ba
Robotgiggle <purplemrb12@gmail.com>
2025-03-03 12:45:12 -0500

    Blade trail effect (surprisingly easy!)

---
+++ Scripts/blade_trail.gd
@@ -0,0 +1,12 @@
+extends Line2D
+
+@export
+var trail_length: int
+var offset = Vector2(-576, -384) # fixes viewport jank
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if get_parent().blade:
+		add_point(get_viewport().get_mouse_position() + offset)
+		if points.size() > trail_length:
+			remove_point(0)
