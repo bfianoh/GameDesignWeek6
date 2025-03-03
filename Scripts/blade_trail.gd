5b81541fef252f1379b6e740be8d325f23004c8a
Robotgiggle <purplemrb12@gmail.com>
2025-03-03 17:10:35 -0500

    Fix warnings (again)

--- Scripts/blade_trail.gd
+++ Scripts/blade_trail.gd
@@ -5,7 +5,7 @@ var trail_length: int
 var offset = Vector2(-576, -384) # fixes viewport jank
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if get_parent().blade:
 		add_point(get_viewport().get_mouse_position() + offset)
 		if points.size() > trail_length:
