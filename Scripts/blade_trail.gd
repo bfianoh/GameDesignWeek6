fd9020a8e24d051eb5affe0590084edd0a99b847
Robotgiggle <purplemrb12@gmail.com>
2025-03-04 15:57:36 -0500

    Fix fonts and cursor for export

--- Scripts/blade_trail.gd
+++ Scripts/blade_trail.gd
@@ -2,11 +2,10 @@ extends Line2D
 
 @export
 var trail_length: int
-var offset = Vector2(-576, -384) # fixes viewport jank
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if get_parent().blade:
-		add_point(get_viewport().get_mouse_position() + offset)
+		add_point(get_local_mouse_position())
 		if points.size() > trail_length:
 			remove_point(0)
