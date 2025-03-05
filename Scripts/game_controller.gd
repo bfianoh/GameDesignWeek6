78257fcadb154069860d4ba317a8f83317fc28b6
Robotgiggle <purplemrb12@gmail.com>
2025-03-05 16:07:09 -0500

    Cleanup

--- Scripts/game_controller.gd
+++ Scripts/game_controller.gd
@@ -15,11 +15,6 @@ var particles: bool = false
 var sounds: bool = false
 var blade: bool = false
 
-# Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#pass
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var explosion = $Explosion
